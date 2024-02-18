36bb88d3feb673a085d75607a69951f34aa8f8f2
Kadir Lofca <kadirlofca@outlook.com>
2024-02-18 18:45:51 -0500

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,97 @@
-use std::fs;
 use std::error::Error;
+use std::fs;
+use std::env;
 
 pub struct Config {
     query: String,
-    path: String
+    path: String,
+    case_insensitive: bool
 }
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("Not enough arguments!!")
+            return Err("Not enough arguments!!");
         }
 
-        // Clone is less efficient than using references, but it doesn't
-        // really matter in this instance.
-        let query = args[1].clone();
-        let path = args[2].clone();
-
-        Ok(Config { query, path })
+        Ok(Config {
+            // Clone is less efficient than using references, but it doesn't
+            // really matter in this instance since it only happens once per run, and it's simple.
+            query: args[1].clone(),
+            path: args[2].clone(),
+            case_insensitive: env::var("CASE_INSENSITIVE").is_ok()
+        })
     }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.path)?;
 
-    println!("{content}");
+    let result = if config.case_insensitive {
+        search_case_insensitive(&config.query, &content)
+    }
+    else {
+        search(&config.query, &content)
+    };
+
+    for line in result {
+        println!("{line}");
+    }
 
     Ok(())
 }
 
-#[cfg(tests)]
+#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn one_result() {
         let query = "duct";
-        let content = "\
+        let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.";
 
-        assert_eq!(vec!["safe, fast, productive."], search(query, content));
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
     }
 }
